91a4c97e45baf48d8edb451af3d87504afc8f12e
Antonio de Souza <contato@antoniodesouza.com.br>
2015-10-29 17:09:16 -0200

    Initial Commit

---
+++ dafiti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dafiti
+//
+//  Created by Antonio De Souza on 29/10/15.
+//  Copyright © 2015 Antonio De Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
